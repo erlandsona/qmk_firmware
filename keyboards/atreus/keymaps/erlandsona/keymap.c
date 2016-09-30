6d326198a150d16e6e79eaa186e89432c08f17c5
Austin Erlandson <austin.erlandson@metova.com>
2016-09-30 11:23:27 -0500

    Updated number row for easier NUM Layer...

--- keyboards/atreus/keymaps/erlandsona/keymap.c
+++ keyboards/atreus/keymaps/erlandsona/keymap.c
@@ -18,15 +18,15 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP( /* Qwerty */
-  KC_Q       , KC_W   , KC_E   , KC_R   ,  KC_T   ,                 KC_Y  ,  KC_U   , KC_I   , KC_O   , KC_P          ,
-  KC_A       , KC_S   , KC_D   , KC_F   ,  KC_G   ,                 KC_H  ,  KC_J   , KC_K   , KC_L   , KC_SCLN       ,
-  SFT_T(KC_Z), KC_X   , KC_C   , KC_V   ,  KC_B   ,                 KC_N  ,  KC_M   , KC_COMM, KC_DOT , SFT_T(KC_QUOT),
-  KC_LCTL    , KC_LALT, KC_LALT, KC_LGUI,  KC_BSPC, KC_ESC, KC_ENT, KC_SPC,  F(NUMS), KC_RALT, KC_SLSH, KC_BSLS      ),
+  KC_Q       , KC_W   , KC_E   , KC_R   ,  KC_T   ,                 KC_Y  ,  KC_U   , KC_I   , KC_O   , KC_P           ,
+  KC_A       , KC_S   , KC_D   , KC_F   ,  KC_G   ,                 KC_H  ,  KC_J   , KC_K   , KC_L   , KC_SCLN        ,
+  SFT_T(KC_Z), KC_X   , KC_C   , KC_V   ,  KC_B   ,                 KC_N  ,  KC_M   , KC_COMM, KC_DOT , KC_QUOT        ,
+  KC_LCTL    , KC_LALT, KC_LALT, KC_LGUI,  KC_BSPC, KC_ESC, KC_ENT, KC_SPC,  F(NUMS), KC_RALT, KC_SLSH, SFT_T(KC_BSLS)),
 
 [NUMS] = KEYMAP( /* Numbers / Arrows / Symbols */
-  KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   ,                   KC_LPRN, KC_RPRN, KC_MINS, KC_EQL , KC_LBRC,
-  KC_TAB , KC_5   , KC_6   , KC_7   , KC_8   ,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_RBRC,
-  _______, KC_9   , KC_0   , KC_DOT , KC_COMM,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______,
+  KC_GRV , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,
+  KC_TAB , KC_1   , XXXXXXX, XXXXXXX, KC_6   ,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_LBRC,
+  _______, XXXXXXX, XXXXXXX, KC_DOT , KC_7   ,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_RBRC,
   _______, _______, _______, _______, KC_DEL , F(MOUS), _______, _______, _______, _______, _______, _______),
 
 [MOUS] = KEYMAP( /* Mouse and Media Keys */
