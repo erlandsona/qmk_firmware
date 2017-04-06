11cd7bd1db607ee630325f1cda2ac435d46cf775
Austin Erlandson <austin@erlandson.com>
2017-04-06 10:12:29 -0500

    Keymap for atreus62gst!

--- keyboards/atreus/keymaps/erlandsona/keymap.c
+++ keyboards/atreus/keymaps/erlandsona/keymap.c
@@ -21,28 +21,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q       , KC_W   , KC_E   , KC_R   ,  KC_T   ,                 KC_Y  ,  KC_U   , KC_I   , KC_O   , KC_P           ,
   KC_A       , KC_S   , KC_D   , KC_F   ,  KC_G   ,                 KC_H  ,  KC_J   , KC_K   , KC_L   , KC_SCLN        ,
   SFT_T(KC_Z), KC_X   , KC_C   , KC_V   ,  KC_B   ,                 KC_N  ,  KC_M   , KC_COMM, KC_DOT , KC_QUOT        ,
-  KC_LCTL    , KC_LALT, KC_LALT, KC_LGUI,  KC_BSPC, KC_ESC, KC_ENT, KC_SPC,  F(NUMS), KC_RALT, KC_SLSH, SFT_T(KC_BSLS)),
+  KC_LCTL    , KC_LALT, KC_LALT, KC_LGUI,  KC_BSPC, KC_ESC, KC_ENT, KC_SPC,  MO(NUMS), KC_RALT, KC_SLSH, SFT_T(KC_BSLS)),
 
 [NUMS] = KEYMAP( /* Numbers / Arrows / Symbols */
   KC_GRV , KC_2   , KC_3   , KC_4   , KC_5   ,                   KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL ,
   KC_TAB , KC_1   , XXXXXXX, XXXXXXX, KC_6   ,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, KC_LBRC,
   _______, XXXXXXX, XXXXXXX, KC_DOT , KC_7   ,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END , KC_RBRC,
-  _______, _______, _______, _______, KC_DEL , F(MOUS), _______, _______, _______, _______, _______, _______),
+  _______, _______, _______, _______, KC_DEL , TO(MOUS), _______, _______, _______, _______, _______, _______),
 
 [MOUS] = KEYMAP( /* Mouse and Media Keys */
   KC_SLCK, KC_PAUSE, KC_F11 , KC_F10 , KC_F9  ,                   KC_F8  , KC_F7  , KC_F6  , KC_F5  , KC_F4,
   KC_VOLD, KC_ACL0 , KC_ACL1, KC_ACL2, KC_VOLU,                   KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_F3,
   KC_MUTE, KC_MPRV , KC_MPLY, KC_MNXT, KC_MUTE,                   KC_WH_R, KC_WH_U, KC_WH_D, KC_WH_L, KC_F2,
-  _______, _______ , _______, _______, _______, _______, _______, KC_BTN1, F(BASE), RESET  , KC_F12 , KC_F1)
+  _______, _______ , _______, _______, _______, TO(BASE), _______, KC_BTN1, TO(BASE), RESET  , KC_F12 , KC_F1)
 };
 
 
-// I prefer this layer switching strategy to the TG and MO functions.
-// so that I can get out of mouse mode just by tapping/holding my base layer FN key.
 const uint16_t PROGMEM fn_actions[] = {
-  [BASE] = ACTION_LAYER_OFF(2, 1),  // switch back to layer 0
-  [NUMS] = ACTION_LAYER_MOMENTARY(1),  // to Fn overlay
-  [MOUS] = ACTION_LAYER_ON(2, 1)  // switch to layer 2
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
